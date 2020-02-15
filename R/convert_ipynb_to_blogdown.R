9d91e4858b2467568a7b421bcb38843c0a6f4f07
Corrie <corrie.b@web.de>
2020-02-15 15:52:41 +0100

    make devtools::check pass

--- R/convert_ipynb_to_blogdown.R
+++ R/convert_ipynb_to_blogdown.R
@@ -36,8 +36,9 @@ remove_pngs <- function(md_text) {
 #'
 #' This function adds a cell calling the reticulate library
 #' to the beginning of the markdown text.
-#' @param python_path python path as string for reticulate. Defaults to the one detected by \code{\link{reticulate::py_config()}}
-#' @param chunk_options string. Passes options to \code{\link{knitr::opts_chunk$set()}}
+#' @param md_text Markdown text as character string to which to attach the reticulate cell
+#' @param python_path python path as string for reticulate. Defaults to the one detected by \code{\link[reticulate:py_config]{reticulate::py_config()}}
+#' @param chunk_options string. Passes options to \code{\link[knitr:opts_chunk]{knitr::opts_chunk$set()}}
 add_reticulate <- function(md_text, python_path = reticulate::py_config()$python, chunk_options="") {
   reticulate_cell <- glue::glue("```{{r, include=FALSE}}
   knitr::opts_chunk$set({chunk_options})
@@ -53,8 +54,8 @@ add_reticulate <- function(md_text, python_path = reticulate::py_config()$python
 #' Converts a markdown file containing python code chunks to a
 #' Rmd file with a reticulate chunk. Returns the Rmd string without YAML header
 #' @param md_file path to the markdown file
-#' @param chunk_options string. Passes options to \code{\link{knitr::opts_chunk$set()}}. Defaults to ""
-#' @param python_path python path as string for reticulate. Defaults to the one detected by \code{\link{reticulate::py_config()}}
+#' @param chunk_options string. Passes options to \code{\link[knitr:opts_chunk]{knitr::opts_chunk$set()}}. Defaults to ""
+#' @param python_path python path as string for reticulate. Defaults to the one detected by \code{\link[reticulate:py_config]{reticulate::py_config()}}
 md_to_Rmd_text <- function(md_file,
                            chunk_options = "",
                            python_path = reticulate::py_config()$python) {
@@ -74,7 +75,7 @@ ipynb_to_md <- function(ipynb_file) {
   file_name <- basename(ipynb_file)
   output_filename <- stringr::str_replace(file_name, ".ipynb", "")
   temp_file <- file.path(dir_name, glue::glue("temp_{file_name}") )
-  system(glue::glue("cat '{ipynb_file}' | nbstripout > '{temp_file}'") )
+  system(glue::glue("cat '{ipynb_file}' | nbstripout > '{temp_file}'"))
   system2("jupyter", args = c("nbconvert",
                               glue::glue("--output '{output_filename}'"),
                               glue::glue("--to markdown '{temp_file}'"), "--ClearMetadataPreprocessor.enabled=True", "--ClearOutput.enabled=True"))
@@ -86,8 +87,9 @@ ipynb_to_md <- function(ipynb_file) {
 #'
 #' Converts a jupyter notebook to Rmd style md. Returns the string without YAML header.
 #' @param ipynb_file path to the jupyter notebook
-#' @param chunk_options string. Passes options to \code{\link{knitr::opts_chunk$set()}}. Defaults to ""
-#' @param python_path python path as string for reticulate. Defaults to the one detected by \code{\link{reticulate::py_config()}}
+#' @param chunk_options string. Passes options to \code{\link[knitr:opts_chunk]{knitr::opts_chunk$set()}}. Defaults to ""
+#' @param python_path python path as string for reticulate. Defaults to the
+#' one detected by \code{\link[reticulate:py_config]{reticulate::py_config()}}
 ipynb_to_Rmd_text <- function(ipynb_file,
                               chunk_options = "",
                               python_path = reticulate::py_config()$python) {
@@ -98,10 +100,11 @@ ipynb_to_Rmd_text <- function(ipynb_file,
 #' Creates a new Blogdown Post from a Jupyter Notebook
 #'
 #' Creates a new blogdown post and appends the jupyter
-#' notebook content as Rmd-style text.
+#' notebook content as Rmd-style text. The notebook
+#' gets transformed into a runnable Rmd with python chunks.
 #' @param ipynb_file path to the jupyter notebook
-#' @param chunk_options global chunk options as charachter vector. Passes options to \code{\link{knitr::opts_chunk$set()}}. Defaults to ""
-#' @param python_path python path as string for reticulate. Defaults to the one detected by \code{\link{reticulate::py_config()}}
+#' @param chunk_options global chunk options as charachter vector. Passes options to \code{\link[knitr:opts_chunk]{knitr::opts_chunk$set()}}. Defaults to ""
+#' @param python_path python path as string for reticulate. Defaults to the one detected by \code{\link[reticulate:py_config]{reticulate::py_config()}}
 #' @param title title of the markdown. Defaults to the name of the notebook
 #' @param kind content type to create
 #' @param open whether to open the generated file after creation
@@ -115,7 +118,7 @@ ipynb_to_Rmd_text <- function(ipynb_file,
 #' @param subdir if specified the post will be generated in a subdirectoy of 'content/'
 #' @param ext extension of the generated file. Defaults to .Rmd
 #' @return Returns the file name of the newly generated file.
-#' @seealso [blogdown::new_post()] which this function wraps.
+#' @seealso \code{\link[blogdown:new_post]{blogdown::new_post()}} for which this function is a wrapper.
 #' @export
 new_post_from_ipynb <- function(ipynb_file, chunk_options = "", python_path = reticulate::py_config()$python,
                                 title="", kind = "", open = interactive(), author = getOption("blogdown.author"),
@@ -134,16 +137,21 @@ new_post_from_ipynb <- function(ipynb_file, chunk_options = "", python_path = re
   write(generated_rmd, file=new_post_file, append = TRUE)
 
   if (open)
-    blogdown:::open_file(new_post_file)
+    open_file(new_post_file)
   new_post_file
 
+}
+
+open_file <- function(x) {
+  tryCatch(rstudioapi::navigateToFile(x), error = function(e) utils::file.edit(x))
 }
 
 #' Creates an Rmd file from a Jupyter Notebook
 #'
 #' @param ipynb_file path to the jupyter notebook
 #' @param chunk_options global chunk options as charachter vector. Passes options to kitr::opts_chunk$set(). Defaults to ""
-#' @param python_path python path as string for reticulate. Defaults to the one detected by \code{\link{reticulate::py_config()}}
+#' @param python_path python path as string for reticulate. Defaults to the one
+#' detected by \code{\link[reticulate:py_config]{reticulate::py_config()}}
 #' @param open whether to open the generated file after creation
 #' @param title title of the markdown as string. Defaults to ""
 #' @param author author as string. Defaults to ""
@@ -159,7 +167,7 @@ Rmd_from_ipynb <- function(ipynb_file, chunk_options = "", python_path = reticul
   write(rmd_text, file=output_filename)
 
   if (open)
-    blogdown:::open_file(output_filename)
+    open_file(output_filename)
   output_filename
 
 }
